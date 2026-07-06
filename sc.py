c23ce788d862ba3bfdc223aecda2c150ed5d8872
milad <seyeadmiladnouri@gmail.com>
2026-07-06 15:10:41 +0330

    scrawler

---
+++ sc.py
@@ -0,0 +1,24 @@
+import requests
+from pyexpat import features
+
+r = requests.get("https://testinno.ir/toefl")
+# print(r)
+# print(type(r))
+# print(r.text)
+# data = 'history' in r.text
+# print(data)
+import bs4
+data = r.text
+soup = bs4.BeautifulSoup(data,'html.parser')
+# print(soup)
+ti = soup.select("title")
+# print (ti)
+# print(type(ti))
+
+div = soup.select('div')
+# print(div)
+print(len(div))
+
+for item in soup.select('div'):
+    print(item.text)
+
